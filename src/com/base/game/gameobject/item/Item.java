4b2ee64e6430bc32467f0f7f315ae4238e3b4353
Alex Kunz <a009k781@ku.edu>
2018-04-07 13:53:16 -0500

    Add documentation for the constructor in Item

--- src/com/base/game/gameobject/item/Item.java
+++ src/com/base/game/gameobject/item/Item.java
@@ -6,6 +6,15 @@ public abstract class Item extends GameObject
 {
     protected int despawnTime;
 
+    /**
+     * Generic constructor for an item
+     * @param xPos x-coordinate of the sprite
+     * @param yPos y-coordinate of the sprite
+     * @param width width
+     * @param height height
+     * @param imgPath file path to the image representing the sprite
+     * @param despawnTime the time it takes for a item to dematerialize and be unattainable 
+     */
     protected Item(float xPos, float yPos, int width, int height, String imgPath, int despawnTime){
         init(xPos, yPos, width, height, imgPath);
 

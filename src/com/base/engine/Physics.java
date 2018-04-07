89a3eea201b2818d42585a43ac43a1972544cb8e
Alex Kunz <a009k781@ku.edu>
2018-04-07 14:07:48 -0500

    Simplify checkCollisions functions

--- src/com/base/engine/Physics.java
+++ src/com/base/engine/Physics.java
@@ -11,9 +11,8 @@ public class Physics {
      */
     public static boolean checkCollision(GameObject obj1, GameObject obj2) {
         Rectangle r1 = new Rectangle((int) (obj1.getX() - (obj1.getWidth()/2.0f)), (int) (obj1.getY()  - (obj1.getHeight()/2.0f)), obj1.getWidth(), obj1.getHeight());
-        Rectangle r2 = new Rectangle((int) (obj2.getX() - (obj2.getWidth()/2.0f)), (int) (obj2.getY()  - (obj2.getHeight()/2.0f)), obj2.getWidth(), obj2.getHeight());
 
-        return r1.intersects(r2);
+        return checkCollision(r1, obj2);
     }
 
     /**

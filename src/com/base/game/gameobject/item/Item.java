c0ec3f7a5ed494fabb35651269c0b0c9cb2f7920
Alex Kunz <a009k781@ku.edu>
2018-04-07 13:50:16 -0500

    Add despawn time for item, fix formatting

--- src/com/base/game/gameobject/item/Item.java
+++ src/com/base/game/gameobject/item/Item.java
@@ -2,8 +2,13 @@ package com.base.game.gameobject.item;
 
 import com.base.engine.GameObject;
 
-public abstract class Item extends GameObject{
-    public Item(float xPos, float yPos, int width, int height, String imgPath){
+public abstract class Item extends GameObject
+{
+    protected int despawnTime;
+
+    protected Item(float xPos, float yPos, int width, int height, String imgPath, int despawnTime){
         init(xPos, yPos, width, height, imgPath);
-    } 
+
+        this.despawnTime = despawnTime;
+    }
 }

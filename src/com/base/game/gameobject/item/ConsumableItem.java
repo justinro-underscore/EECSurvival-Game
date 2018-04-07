4a34c1ada28ef84f672ff9574a2d796a63886d97
Alex Kunz <a009k781@ku.edu>
2018-04-07 15:35:06 -0500

    Create ConsumableItem class that inherits from Item

---
+++ src/com/base/game/gameobject/item/ConsumableItem.java
@@ -0,0 +1,26 @@
+package com.base.game.gameobject.item;
+
+public class ConsumableItem extends Item
+{
+    private int addedHealth;
+
+    /**
+     * Constructor for a consumable item
+     * @param xPos x-coordinate of the sprite
+     * @param yPos y-coordinate of the sprite
+     * @param width width
+     * @param height height
+     * @param imgPath file path to the image representing the sprite
+     * @param despawnTime the time it takes for a item to dematerialize and be unattainable
+     * @param addedHealth the health that will be added to the character who collides with the object
+     */
+    public ConsumableItem(float xPos, float yPos, int width, int height, String imgPath, int despawnTime, int addedHealth){
+        super(xPos, yPos, width, height, imgPath, despawnTime);
+
+        this.addedHealth = addedHealth;
+    }
+
+    public int getAddedHealth(){
+        return this.addedHealth;
+    }
+}

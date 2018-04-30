805b47df0e11366fc027662b8584eb014d63f1a6
Varun Chadha <vchadha023@gmail.com>
2018-04-29 19:09:16 -0500

    Added overloaded test level - vchadha

--- src/com/test/TestLevel.java
+++ src/com/test/TestLevel.java
@@ -1,8 +1,30 @@
 package com.test;
 
+import com.base.engine.Display;
+import com.base.game.gameobject.entity.Player;
 import com.base.game.levels.Level;
+
+import java.awt.Robot;
 
 //TODO: implement test level with one of every item -> have player randomly move in directions (if die - respawn)
 //Key log and print to file if fail
 public class TestLevel extends Level {
+    private boolean isAutomated;
+
+    public TestLevel(boolean isAutomated) {
+        player = new Player(Display.getWidth() / 2 - 30, Display.getHeight() / 2 - 30, 41, 82, "res/assets/player.png", 4f, 20, 5);
+
+        init("res/assets/bricks.jpg", player);
+        this.isAutomated = isAutomated;
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
 }

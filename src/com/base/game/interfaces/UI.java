1bf57120c2461dd40c05d365a17b8980cfcd07d8
justinro-underscore <jcroderman@gmail.com>
2018-05-02 21:32:53 -0500

    Fixes black sprite bug with UI

--- src/com/base/game/interfaces/UI.java
+++ src/com/base/game/interfaces/UI.java
@@ -8,12 +8,17 @@ import java.awt.*;
 
 import static org.lwjgl.opengl.GL11.*;
 
-public class UI extends Interface // TODO Add all UI functionality
+public class UI extends Interface
 {
+    private Sprite playerHealthBarBack;
+    private Sprite playerHealthBarFull;
+    private Sprite playerHealthBarCurr;
+    private Sprite bossHealthBarBack;
+    private Sprite bossHealthBarFull;
+    private Sprite bossHealthBarCurr;
     private Rectangle playerHealthBar;
     private Animation theDigit;
     private float playerHealthFactor;
-    private SpriteRetriever retriever;
     private Rectangle bossHealthBar;
     private float bossHealthFactor;
     private final int PLAYER_HEALTH_BAR_WIDTH = Display.getWidth() / 2 - 20;
@@ -29,8 +34,14 @@ public class UI extends Interface // TODO Add all UI functionality
      */
     public UI(int playerMaxHealth, int bossMaxHealth)
     {
+        playerHealthBarBack = SpriteRetriever.loadSprite("res/assets/blue.png", 0, 0, 1, 50, 50)[0];
+        playerHealthBarFull = SpriteRetriever.loadSprite("res/assets/black.png", 0, 0, 1, 50, 50)[0];
+        playerHealthBarCurr = SpriteRetriever.loadSprite("res/assets/red.png", 0, 0, 1, 50, 50)[0];
         playerHealthBar = new Rectangle(10, 10, PLAYER_HEALTH_BAR_WIDTH, 20);
         playerHealthFactor = (float)PLAYER_HEALTH_BAR_WIDTH / playerMaxHealth;
+        bossHealthBarBack = SpriteRetriever.loadSprite("res/assets/blue.png", 0, 0, 1, 50, 50)[0];
+        bossHealthBarFull = SpriteRetriever.loadSprite("res/assets/black.png", 0, 0, 1, 50, 50)[0];
+        bossHealthBarCurr = SpriteRetriever.loadSprite("res/assets/red.png", 0, 0, 1, 50, 50)[0];
         bossHealthBar = new Rectangle(20, Display.getHeight() - 60, BOSS_HEALTH_BAR_WIDTH, 40);
         bossHealthFactor = (float)BOSS_HEALTH_BAR_WIDTH / bossMaxHealth;
 
@@ -106,35 +117,9 @@ public class UI extends Interface // TODO Add all UI functionality
      */
     private void showPlayerHealth()
     {
-        // Health remaining
-        glColor4f(1, 0, 0, 0.0f);
-        glBegin(GL11.GL_QUADS);
-            glVertex2f(playerHealthBar.x, playerHealthBar.y);
-            glVertex2f(playerHealthBar.x + playerHealthBar.width, playerHealthBar.y);
-            glVertex2f(playerHealthBar.x + playerHealthBar.width, playerHealthBar.y + playerHealthBar.height);
-            glVertex2f(playerHealthBar.x, playerHealthBar.y + playerHealthBar.height);
-        glEnd();
-
-        // Health taken
-        glColor4f(0, 0, 0, 0.0f);
-        glBegin(GL11.GL_QUADS);
-            glVertex2f(playerHealthBar.x + playerHealthBar.width, playerHealthBar.y);
-            glVertex2f(playerHealthBar.x + PLAYER_HEALTH_BAR_WIDTH, playerHealthBar.y);
-            glVertex2f(playerHealthBar.x + PLAYER_HEALTH_BAR_WIDTH, playerHealthBar.y + playerHealthBar.height);
-            glVertex2f(playerHealthBar.x + playerHealthBar.width, playerHealthBar.y + playerHealthBar.height);
-        glEnd();
-
-        // Health bar outline
-        glLineWidth(5);
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        glColor4f(0, 0, 1, 0.0f);
-        glBegin(GL11.GL_QUADS);
-            glVertex2f(playerHealthBar.x - 2, playerHealthBar.y - 2);
-            glVertex2f(playerHealthBar.x + PLAYER_HEALTH_BAR_WIDTH + 2, playerHealthBar.y - 2);
-            glVertex2f(playerHealthBar.x + PLAYER_HEALTH_BAR_WIDTH + 2, playerHealthBar.y + playerHealthBar.height + 2);
-            glVertex2f(playerHealthBar.x - 2, playerHealthBar.y + playerHealthBar.height + 2);
-        glEnd();
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        playerHealthBarBack.render(playerHealthBar.x - 4, playerHealthBar.y - 4, PLAYER_HEALTH_BAR_WIDTH + 8, playerHealthBar.height + 8); // Health bar outline
+        playerHealthBarFull.render(playerHealthBar.x, playerHealthBar.y, PLAYER_HEALTH_BAR_WIDTH, playerHealthBar.height); // Health taken
+        playerHealthBarCurr.render(playerHealthBar.x, playerHealthBar.y, playerHealthBar.width, playerHealthBar.height); // Health remaining
     }
 
     /**
@@ -142,35 +127,9 @@ public class UI extends Interface // TODO Add all UI functionality
      */
     private void showBossHealth()
     {
-        // Health remaining
-        glColor4f(1, 0, 0, 0.0f);
-        glBegin(GL11.GL_QUADS);
-            glVertex2f(bossHealthBar.x, bossHealthBar.y);
-            glVertex2f(bossHealthBar.x + bossHealthBar.width, bossHealthBar.y);
-            glVertex2f(bossHealthBar.x + bossHealthBar.width, bossHealthBar.y + bossHealthBar.height);
-            glVertex2f(bossHealthBar.x, bossHealthBar.y + bossHealthBar.height);
-        glEnd();
-
-        // Health taken
-        glColor4f(0, 0, 0, 0.0f);
-        glBegin(GL11.GL_QUADS);
-            glVertex2f(bossHealthBar.x + bossHealthBar.width, bossHealthBar.y);
-            glVertex2f(bossHealthBar.x + BOSS_HEALTH_BAR_WIDTH, bossHealthBar.y);
-            glVertex2f(bossHealthBar.x + BOSS_HEALTH_BAR_WIDTH, bossHealthBar.y + bossHealthBar.height);
-            glVertex2f(bossHealthBar.x + bossHealthBar.width, bossHealthBar.y + bossHealthBar.height);
-        glEnd();
-
-        // Health bar outline
-        glLineWidth(5);
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        glColor4f(0, 0, 1, 0.0f);
-        glBegin(GL11.GL_QUADS);
-            glVertex2f(bossHealthBar.x - 2, bossHealthBar.y - 2);
-            glVertex2f(bossHealthBar.x + BOSS_HEALTH_BAR_WIDTH + 2, bossHealthBar.y - 2);
-            glVertex2f(bossHealthBar.x + BOSS_HEALTH_BAR_WIDTH + 2, bossHealthBar.y + bossHealthBar.height + 2);
-            glVertex2f(bossHealthBar.x - 2, bossHealthBar.y + bossHealthBar.height + 2);
-        glEnd();
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        bossHealthBarBack.render(bossHealthBar.x - 4, bossHealthBar.y - 4, BOSS_HEALTH_BAR_WIDTH + 8, bossHealthBar.height + 8); // Health bar outline
+        bossHealthBarFull.render(bossHealthBar.x, bossHealthBar.y, BOSS_HEALTH_BAR_WIDTH, bossHealthBar.height); // Health taken
+        bossHealthBarCurr.render(bossHealthBar.x, bossHealthBar.y, bossHealthBar.width, bossHealthBar.height); // Health remaining
     }
 
     /**
@@ -178,19 +137,10 @@ public class UI extends Interface // TODO Add all UI functionality
      */
     private void showPlayerHealthNumber()
     {
-        // Background on the text
-//        glColor4f(1, 1, 1, 0.0f);
-//        glBegin(GL11.GL_QUADS);
-//            glVertex2f(playerHealthBar.x + PLAYER_HEALTH_BAR_WIDTH + 20, playerHealthBar.y);
-//            glVertex2f(playerHealthBar.x + PLAYER_HEALTH_BAR_WIDTH + 95, playerHealthBar.y);
-//            glVertex2f(playerHealthBar.x + PLAYER_HEALTH_BAR_WIDTH + 95, playerHealthBar.y + 25);
-//            glVertex2f(playerHealthBar.x + PLAYER_HEALTH_BAR_WIDTH + 20, playerHealthBar.y + 25);
-//        glEnd();
-
         // Shows the player health digits
-      for(int i = 0; i < 3; i++)
+        for(int i = 0; i < 3; i++)
         {
-            playerHealthDigits[i].render(playerHealthBar.x + PLAYER_HEALTH_BAR_WIDTH + (25 * i), playerHealthBar.y,25,25);
+            playerHealthDigits[i].render(playerHealthBar.x + PLAYER_HEALTH_BAR_WIDTH + (25 * i) + 5, playerHealthBar.y - 3,25,25);
         }
     }
 }
